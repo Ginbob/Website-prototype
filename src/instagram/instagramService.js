c2fb0494bafe666f91159d1d34230b00acfe1b23
henning <henning.nobbe@innoq.com>
2020-12-16 13:13:34 +0100

    try rss feed

--- src/instagram/instagramService.js
+++ src/instagram/instagramService.js
@@ -54,6 +54,9 @@ function convertPostData(postNode) {
 module.exports.scrapeLarsProfile = async () => {
     const igPosts = [];
     try {
+        console.log("----------");
+        await this.requestRssFeed();
+        console.log("----------");
         const cachedContent = myCache.get(instaFeed);
         if (cachedContent) {
             console.log("serving ig feed from cache");
@@ -62,10 +65,10 @@ module.exports.scrapeLarsProfile = async () => {
             // const options = {count: 10, mediaType: 'image'};
             console.log("try fetching ig feed");
             // const user = await instaTouch.user('lar_alt', options);
-            const user = await this.requestIgUser();
-            igPosts.push(...convertUserData(user));
-            myCache.set(instaFeed, igPosts);
-            console.log("fetched ig feed");
+            // const user = await this.requestIgUser();
+            // igPosts.push(...convertUserData(user));
+            // myCache.set(instaFeed, igPosts);
+            // console.log("fetched ig feed");
             return igPosts;
         }
     } catch (error) {
@@ -106,4 +109,20 @@ module.exports.requestIgUser = async () => {
     };
     const response = await axios(options);
     return response.data;
+}
+
+module.exports.requestRssFeed = async () => {
+    const options = {
+        "url": "https://rss.app/feeds/qwIw0ptg1iHoH213.xml",
+        "method": "GET",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
+            "Accept": "application/xhtml+xml,application/xml,application/json;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Upgrade-Insecure-Requests": 1
+        }
+    };
+    const response = await axios(options);
+    return console.log(response);
 }
