464985dffd43deb129fa1d5935781605011ad0b9
henning <henning.nobbe@innoq.com>
2020-12-16 11:54:45 +0100

    Try new ig fetch

--- src/instagram/instagramService.js
+++ src/instagram/instagramService.js
@@ -1,6 +1,7 @@
 const instaTouch = require("instatouch");
 const dateFormat = require('dateformat');
 const NodeCache = require('node-cache');
+const axios = require('axios');
 
 /**
  * Google API calls cost money and it's not really important if the results are stale. There for we want to cache them, in memory should be enough for now.
@@ -58,10 +59,11 @@ module.exports.scrapeLarsProfile = async () => {
             console.log("serving ig feed from cache");
             return cachedContent;
         } else {
-            const options = {count: 10, mediaType: 'image'};
+            // const options = {count: 10, mediaType: 'image'};
             console.log("try fetching ig feed");
-            const user = await instaTouch.user('lar_alt', options);
-            igPosts.push(...user.collector.map(convertPostData));
+            // const user = await instaTouch.user('lar_alt', options);
+            const user = await this.requestIgUser();
+            igPosts.push(...convertUserData(user));
             myCache.set(instaFeed, igPosts);
             console.log("fetched ig feed");
             return igPosts;
@@ -71,4 +73,37 @@ module.exports.scrapeLarsProfile = async () => {
         console.log(error);
         return [];
     }
+}
+
+function convertUserData(user) {
+    const posts = user.graphql.user.edge_owner_to_timeline_media.edges;
+    return posts.map(post => {
+        const postNode = post.node
+        const description = cutDescription(post.node.edge_media_to_caption.edges[0].node.text);
+        return {
+            mediaId: postNode.id,
+            shortcode: postNode.shortcode,
+            thumbnail: postNode.thumbnail_src,
+            date: getDateFormatted(postNode.taken_at_timestamp),
+            text: description,
+            isVideo: postNode.is_video,
+            videoUrl: postNode.video_url || undefined
+        };
+    })
+}
+
+module.exports.requestIgUser = async () => {
+    const options = {
+        "url": "https://www.instagram.com/lar_alt/?__a=1",
+        "method": "GET",
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36",
+            "Accept": "application/xhtml+xml,application/xml,application/json;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Upgrade-Insecure-Requests": 1
+        }
+    };
+    const response = await axios(options);
+    return response.data;
 }
